49c0227996efb78c6c24fc7eb2b7f5ef6c09a314
Justin Moore <Justin.Moore@purplebricks.com>
2019-04-21 13:50:18 -0400

    implicit typing and loop review

---
+++ Chapter3/Chapter3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DeclareImplicitVars();
+            // LinqQueryOverInts();
+            DoWhileLoopExample();
+
+            Console.ReadLine();
+        }
+
+        private static void DoWhileLoopExample()
+        {
+            string userIsDone;
+            do
+            {
+                Console.WriteLine("In do/while loop");
+                Console.Write("Are you done? [yes] [no]: ");
+                userIsDone = Console.ReadLine();
+            } while (userIsDone.ToLower() != "yes"); // note the semicolon
+        }
+
+        private static void LinqQueryOverInts()
+        {
+            int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
+            // LINQ Query
+            var subset = from i in numbers where i < 10 select i;
+            Console.Write("Values in subset: ");
+            foreach (var i in subset)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+
+        private static void DeclareImplicitVars()
+        {
+            // Implicitly typed local vars
+            var myInt = 0;
+            var myBool = true;
+            var myString = "Time, marches on...";
+
+            // Print out the underlying type using reflection
+            Console.WriteLine("myInt is a: {0}", myInt.GetType().Name);
+            Console.WriteLine("myBool is a: {0}", myBool.GetType().Name);
+            Console.WriteLine("myString is a: {0}", myString.GetType().Name);
+
+        }
+    }
+}
