a4b039303eb2971c20aff60ef1f9d50c1dffe71b
Justin Moore <Justin.Moore@purplebricks.com>
2019-05-25 17:02:00 -0400

    Local functions, start enums

---
+++ Chapter4/FunWithEnums/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithEnums
+{
+    enum EmployeeType : byte
+    {
+        Manager = 102,
+        Grunt,
+        Contractor,
+        VicePresident
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Fun with Enums ***");
+            // Make an EmployeeType variable.
+            EmployeeType emp = EmployeeType.Contractor;
+            AskForBonus(emp);
+            Console.ReadLine();
+        }
+
+        private static void AskForBonus(EmployeeType emp)
+        {
+            switch (emp)
+            {
+                case EmployeeType.Manager:
+                    Console.WriteLine("How about stock options instead?");
+                    break;
+                case EmployeeType.Grunt:
+                    Console.WriteLine("You have got to be kidding...");
+                    break;
+                case EmployeeType.Contractor:
+                    Console.WriteLine("You already have enough cash...");
+                    break;
+                case EmployeeType.VicePresident:
+                    Console.WriteLine("Very good, sir!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Chapter4/FunWithLocalFunctions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithLocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static int AddWrapper(int x, int y)
+        {
+            // Do some validation here
+            return Add();
+
+            int Add() => x + y;
+        }
+
+    }
+}
