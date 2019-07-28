a2c538d7e83aa9eb7ee274cd4493a78d55d7d9fc
SirJUST <theoperaking@yahoo.com>
2019-07-28 10:40:05 -0400

    Constructors and static members

--- Chapter5/SimpleClassExample/Motorcyle.cs
+++ Chapter5/SimpleClassExample/Motorcyle.cs
@@ -9,28 +9,21 @@ namespace SimpleClassExample
     class Motorcyle
     {
         public int driverIntensity;
-
         public string driverName;
+
         public void SetDriverName(string name)
         {
             driverName = name;
         }
         public void PopAWheely()
         {
-            Console.WriteLine("Yeeeeeee Haaaaaeewww!");
+            for(int i = 0; i <= driverIntensity; i++)
+            {
+                Console.WriteLine("Yeeeeeee Haaaaaeewww!");
+            }
         }
 
-        // put back the default constructor, which will set all data members to default values
-        public Motorcyle() { }
-
-        // our custom constructor
-        public Motorcyle(int intensity)
-            : this(intensity, "") { }
-        public Motorcyle(string name)
-            : this(0, name) { }
-
-        // this is the master constructor that does all the real work
-        public Motorcyle(int intensity, string name)
+        public Motorcyle(int intensity = 0, string name = "")
         {
             if (intensity > 10)
             {
@@ -39,5 +32,37 @@ namespace SimpleClassExample
             driverIntensity = intensity;
             driverName = name;
         }
+
+        // The following lines are an example of constructor chaining
+
+        //// put back the default constructor, which will set all data members to default values
+        //public Motorcyle()
+        //{
+        //    Console.WriteLine("In default ctor");
+        //}
+
+        //// our custom constructor
+        //public Motorcyle(int intensity)
+        //    : this(intensity, "")
+        //{
+        //    Console.WriteLine("In ctor taking an int");
+        //}
+        //public Motorcyle(string name)
+        //    : this(0, name)
+        //{
+        //    Console.WriteLine("In ctor taking a string");
+        //}
+
+        //// this is the master constructor that does all the real work
+        //public Motorcyle(int intensity, string name)
+        //{
+        //    Console.WriteLine("In master ctor");
+        //    if (intensity > 10)
+        //    {
+        //        intensity = 10;
+        //    }
+        //    driverIntensity = intensity;
+        //    driverName = name;
+        //}
     }
 }
--- Chapter5/SimpleClassExample/Program.cs
+++ Chapter5/SimpleClassExample/Program.cs
@@ -10,11 +10,55 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** Fun with Class Types ***\n");
-
-            InstantiateMultipleCars();
+            // Console.WriteLine("*** Fun with Class Types ***\n");
+            MakeAccounts();
+            // MakeSomeBikes();
+            // InstantiateMotorcycles();
+            // InstantiateMultipleCars();
             // FirstCar();
             Console.ReadLine();
+        }
+
+        private static void MakeAccounts()
+        {
+            Console.WriteLine("*** Fun with Static Data ***\n");
+            SavingsAccount s1 = new SavingsAccount(50);
+            SavingsAccount s2 = new SavingsAccount(100);
+
+            // Print the current interest rate
+            Console.WriteLine($"Interest rate is {SavingsAccount.GetInterestRate()}");
+
+            SavingsAccount.SetInterestRate(0.08);
+
+            // Make new object, doesn't reset the interest rate
+            SavingsAccount s3 = new SavingsAccount(10000.75);
+            Console.WriteLine($"Interest Rate is {SavingsAccount.GetInterestRate()}");
+        }
+
+        private static void MakeSomeBikes()
+        {
+            // driverName = "", driverIntensity = 0
+            Motorcyle m1 = new Motorcyle();
+            Console.WriteLine($"Name: {m1.driverName}, Intensity: {m1.driverIntensity}");
+
+            // driverName = "Tiny", driverIntensity = 0
+            Motorcyle m2 = new Motorcyle(name:"Tiny");
+            Console.WriteLine($"Name: {m2.driverName}, Intensity: {m2.driverIntensity}");
+
+            // driverName = "", driverIntensity = 7
+            Motorcyle m3 = new Motorcyle(7);
+            Console.WriteLine($"Name: {m3.driverName}, Intensity: {m3.driverIntensity}");
+        }
+
+        private static void InstantiateMotorcycles()
+        {
+            // This has the same behavior using a constructor with optional arguments as with constructor chaining, minus the WriteLines
+
+            // Make a motorcycle
+            Motorcyle c = new Motorcyle(5);
+            c.SetDriverName("Tiny");
+            c.PopAWheely();
+            Console.WriteLine($"Rider name is {c.driverName}");
         }
 
         private static void InstantiateMultipleCars()
